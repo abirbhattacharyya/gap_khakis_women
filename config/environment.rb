ec2bc810b2b78dbea4ad6bdf913f9dd37a7198c1
dhavalp <dhaval.parikh33@gmail.com>
2011-05-07 15:21:01 +0530

    Sql dump added

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
 #    config.gem "pdf-writer",:lib => "pdf/writer",:version => '1.1.8'
 #    config.gem "gattica",:version => '0.4.1'
       config.gem "i18n",:version => "0.3.6"
+      config.gem "text"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
